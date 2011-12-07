47523755f2c64c4d44c1d32f8c7f4f06ecee8110
Ladislav Prskavec <ladislav.prskavec@gmail.com>
2011-12-07 15:52:18 +0100

    Add upgrade pear

--- data/install_pear.sh
+++ data/install_pear.sh
@@ -3,7 +3,7 @@ sudo pear config-set auto_discover 1
 sudo pear channel-discover pear.netpirates.net
 sudo pear channel-discover components.ez.no
 sudo pear channel-discover pear.docblox-project.org
-
+sudo pear upgrade
 sudo pear install theseer/Autoload
 sudo pear install pear.phpqatools.org/phpqatools PHPDocumentor
 
