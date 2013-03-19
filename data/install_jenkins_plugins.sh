e3792209f9694a2e6112a899ccd2f86c4a8265b8
Ladislav Prskavec <ladislav@prskavec.net>
2013-03-19 11:02:17 +0100

    Update plugins install

--- data/install_jenkins_plugins.sh
+++ data/install_jenkins_plugins.sh
@@ -6,24 +6,10 @@ sudo apt-get install jenkins
 
 wget http://localhost:8080/jnlpJars/jenkins-cli.jar
 
-java -jar jenkins-cli.jar -s http://localhost:8080 install-plugin checkstyle
-java -jar jenkins-cli.jar -s http://localhost:8080 install-plugin cloverphp
-java -jar jenkins-cli.jar -s http://localhost:8080 install-plugin dry
-java -jar jenkins-cli.jar -s http://localhost:8080 install-plugin htmlpublisher
-java -jar jenkins-cli.jar -s http://localhost:8080 install-plugin jdepend
-java -jar jenkins-cli.jar -s http://localhost:8080 install-plugin plot
-java -jar jenkins-cli.jar -s http://localhost:8080 install-plugin pmd
-java -jar jenkins-cli.jar -s http://localhost:8080 install-plugin violations
-java -jar jenkins-cli.jar -s http://localhost:8080 install-plugin xunit
+java -jar jenkins-cli.jar -s http://localhost:8080 install-plugin \
+checkstyle cloverphp dry htmlpublisher jdepend plot pmd violations xunit
 
-java -jar jenkins-cli.jar -s http://localhost:8080 install-plugin git
-java -jar jenkins-cli.jar -s http://localhost:8080 install-plugin greenballs
-java -jar jenkins-cli.jar -s http://localhost:8080 install-plugin jobConfigHistory
-java -jar jenkins-cli.jar -s http://localhost:8080 install-plugin build-pipeline-plugin
-java -jar jenkins-cli.jar -s http://localhost:8080 install-plugin join
-java -jar jenkins-cli.jar -s http://localhost:8080 install-plugin disk-usage
-java -jar jenkins-cli.jar -s http://localhost:8080 install-plugin configurationslicing
-java -jar jenkins-cli.jar -s http://localhost:8080 install-plugin promoted-builds
-java -jar jenkins-cli.jar -s http://localhost:8080 install-plugin email-ext
+java -jar jenkins-cli.jar -s http://localhost:8080 install-plugin \
+git greenballs jobConfigHistory build-pipeline-plugin join disk-usage configurationslicing promoted-builds email-ext
 
 java -jar jenkins-cli.jar -s http://localhost:8080 safe-restart
